be637c11ec78180435eec76f9f86598a9b4f026c
Kashish <kashish.biserwal@protonmail.com>
2024-06-23 11:51:04 +0530

    server init

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(express.static('public'))
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Server Running!')
+})
+
+app.all('*', (_req, res) => {
+    res.status(200).send({
+        status: 404,
+        error: 'Not found',
+        error_description: `(${_req.url}), route or file not found.`,
+    })
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
